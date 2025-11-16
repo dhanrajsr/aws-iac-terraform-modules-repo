45567f1e962e37a8f3ecb48fcfca541ecd287a6f
dhanrajsr <69510417+dhanrajsr@users.noreply.github.com>
2025-11-16 12:35:17 +0530

    Create variables.tf

---
+++ vpc_end_point/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  type = string
+  default = ""
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  default = []
+}
+
+variable "service_name" {
+  type = string
+  default = ""
+}
+
+variable "vpc_endpoint_type" {
+  type = string
+  default = ""
+}
