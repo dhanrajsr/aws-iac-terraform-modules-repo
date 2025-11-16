b45bdda9454cc8e9c4e968392713707803d386b9
dhanrajsr <69510417+dhanrajsr@users.noreply.github.com>
2025-11-16 12:38:15 +0530

    Create main.tf

---
+++ vpc_end_point_interface/main.tf
@@ -0,0 +1,24 @@
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+}
+
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
+resource "aws_vpc_endpoint" "endpoints" {
+  vpc_id            = data.aws_vpc.main.id
+  service_name      = var.service_name
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = data.aws_subnets.private.ids
+  security_group_ids = var.security_group_ids
+  private_dns_enabled = true
+}
